2da846e28bcadf41d5d0b35d4faa6f1f7e0ca5bb
Lukas Selichar <lukas.selichar@adastragrp.com>
2018-06-19 11:15:29 +0200

    init commit

---
+++ src/main/java/cz/legas/brl/BattleRoyaleLarpApplication.java
@@ -0,0 +1,12 @@
+package cz.legas.brl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BattleRoyaleLarpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BattleRoyaleLarpApplication.class, args);
+    }
+}
